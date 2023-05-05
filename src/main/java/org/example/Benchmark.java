473821a70ac36caa6d746777bf61b2c8e805bd29
Martivis <vanek-non@rambler.ru>
2023-05-05 12:31:01 +0300

    Add benchmark

--- src/main/java/org/example/Benchmark.java
+++ src/main/java/org/example/Benchmark.java
@@ -1,2 +1,19 @@
-package org.example;public class Benchmark {
+package org.example;
+
+public class Benchmark {
+    Command command;
+
+    public Benchmark(Command command) {
+        this.command = command;
+    }
+
+    public long Run(int times) {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+        for (int i = 0; i < times; i++) {
+            command.execute();
+        }
+        stopwatch.stop();
+        return stopwatch.getElapsedTime();
+    }
 }
